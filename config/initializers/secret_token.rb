8d37b02457e72e9b2b9f590713df34610140b9a6
Administrator <admin@dti-vps-srv13.(none)>
2010-09-15 11:23:50 +0900

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bk::Application.config.secret_token = 'aab64990f71bf470428651f2f5d906b0cd2066a8e3beee36b57f23688efd2de212ed5a739634c4e87f7adf422e8aec63f5d8118feafffbb91abb44c31c7fb596'
